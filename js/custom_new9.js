17b9ff2eced620f25605129cc92d82eb5a6dd8f4
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-04-03 21:30:50 +0530

    removed toggle

--- js/custom_new9.js
+++ js/custom_new9.js
@@ -929,7 +929,7 @@ function scrollthere(item, event) {
 
   // Switch Btn
   $("body").append(
-    "<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>"
+    // "<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>"
   );
 })(jQuery);
 
